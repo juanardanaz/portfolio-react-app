3c665a3ca82f5bbd60e4b0721a645b82289822b9
Juan <juancruzardanaz@gmail.com>
2022-09-27 00:36:20 -0300

    work and contact sections added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaLinkedin, FaGithub} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import Logo from '../assets/logo_jc.png';
+import {Link} from 'react-scroll';
 
 function Navbar() {
     const [nav, setNav] = useState(false)
@@ -16,11 +17,31 @@ function Navbar() {
 
         {/* Menu */}
         <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li>
+                <Link to="home" smooth={true} duration={500}>
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link to="about" smooth={true} duration={500}>
+                    About
+                </Link>
+            </li>
+            <li>
+                <Link to="skills" smooth={true} duration={500}>
+                    Skills
+                </Link>
+            </li>
+            <li>
+                <Link to="work" smooth={true} duration={500}>
+                    Work
+                </Link>
+            </li>
+            <li>
+                <Link to="contact" smooth={true} duration={500}>
+                    Contact
+                </Link>
+            </li>
         </ul>
 
         {/* Hamburger */}
@@ -30,11 +51,31 @@ function Navbar() {
 
         {/* Mobile Menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Work</li>
-            <li className='py-6 text-4xl'>Contact</li>
+            <li className='py-6 text-4xl'>
+                <Link to="home" smooth={true} duration={500} onClick={handleClick}>
+                    Home
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link to="about" smooth={true} duration={500} onClick={handleClick}>
+                    About
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link to="skills" smooth={true} duration={500} onClick={handleClick}>
+                    Skills
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link to="work" smooth={true} duration={500} onClick={handleClick}>
+                    Work
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link to="contact" smooth={true} duration={500} onClick={handleClick}>
+                    Contact
+                </Link>
+            </li>
         </ul>
 
         {/* Social Icons */}
